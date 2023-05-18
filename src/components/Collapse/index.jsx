c2d883426fb6e985f94f3912513dabf4b1d617fe
Jonathan <cornicjonathan@gmail.com>
2023-05-18 17:59:00 +0200

    parametrage du comportement du composent collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,14 +1,32 @@
 import './Collapse.css'
 import Vector from '../../assets/Vector.svg'
+import { useState } from 'react'
 
-const Collapse = ({title, description}) => {
+const Collapse = ({ title, description }) => {
+    const [toggle, setToggle] = useState(false)
+
+    function ToggleCollapse() {
+        setToggle(!toggle)
+    }
+
     return (
         <div className="collapse-container">
-            <div className='collapse-banner'>
+            <div className="collapse-banner">
                 <div className="collapse-title">{title}</div>
-                <img src={Vector} alt="" className="collapse-vector" />
+                <img
+                    src={Vector}
+                    alt=""
+                    className={`collapse-vector ${toggle ? 'collapsed' : ''}`}
+                    onClick={ToggleCollapse}
+                />
             </div>
-            <div className="collapse-description">{description}</div>
+            {!toggle && (
+                <div
+                    className={`collapse-description ${toggle ? 'collapsed' : ''}`}
+                >
+                    {description}
+                </div>
+            )}
         </div>
     )
 }
