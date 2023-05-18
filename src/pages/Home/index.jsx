b8a4a2d6543989dad26c1c1fdb06c5e64fcbc4a3
Jonathan <cornicjonathan@gmail.com>
2023-05-18 06:26:39 +0200

    nouvelle arborescence et fix css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
-import Banner from '../../components/Banner'
+import Banner from '../../components/headerComponents/Banner'
+import CardContainer from '../../components/mainComponents/CardContainer'
 import bannerImage from '../../assets/banner-home.jpg'
-import CardContainer from '../../components/CardContainer'
 
 const Home = () => {
     return (
